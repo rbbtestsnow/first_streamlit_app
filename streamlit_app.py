44b9e3ec235718f2eefb1c0da12c58a1ed7d99bc
rbbtestsnow <105441078+rbbtestsnow@users.noreply.github.com>
2022-05-18 18:05:45 +0200

    Update streamlit_app.py
    added variable to the fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,18 +49,18 @@ except URLError as e:
 
 import snowflake.connector
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+if streamlit.button('Add a fruit to the list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_cur = my_cnx.cursor()
+   my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+   my_data_rows = my_cur.fetchall()
+   streamlit.dataframe(my_data_rows)
 
 
 def insert_row_snowflake(new_fruit):
    with my_cnx.cursor() as my_cur:
-      my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlint')")
+      my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('"+ new_fruit +"')")
       
       
 # Gather the information from the user 
